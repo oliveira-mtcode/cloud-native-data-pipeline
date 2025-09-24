b04198483262a38c9f609d872452105f7f5d438a
oliveira-mtcode <matheusoliveiradev083@outlook.com>
2025-09-24 19:59:54 +0100

    feat: triger with auth

--- functions/main.py
+++ functions/main.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import os
 
 from src.pipeline.config import gcp, paths
-from src.pipeline.dataproc_job import main as run_dataproc_job
+from src.pipeline.dataproc_job import main as run_dataproc_job, main as run_dataproc_job_with_auth
 
 
 def trigger(request):  # Google Cloud Functions HTTP entrypoint
@@ -11,4 +11,8 @@ def trigger(request):  # Google Cloud Functions HTTP entrypoint
     run_dataproc_job()
     return ("Triggered Dataproc pipeline", 200)
 
+def trigger_with_auth(request):  # Google Cloud Functions HTTP entrypoint
+    _ = request  # unused
+    run_dataproc_job_with_auth()
+    return ("Triggered Dataproc pipeline", 200)
 
